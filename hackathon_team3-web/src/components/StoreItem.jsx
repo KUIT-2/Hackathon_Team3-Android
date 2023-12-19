4d20ea7703a133108dcb753c8e4e34f5e838559b
nangniya <apinksky00@gmail.com>
2023-12-20 02:35:06 +0900

    feat: 홈화면 width 수정

--- hackathon_team3-web/src/components/StoreItem.jsx
+++ hackathon_team3-web/src/components/StoreItem.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import styled from 'styled-components';
+import styled from "styled-components";
 
 import { StarIcon, BookmarkIcon2 } from "../assets/index";
 
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 
 const StoreImage = styled.img`
   width: 180px;
@@ -13,29 +13,27 @@ const StoreImage = styled.img`
 `;
 
 const BookmarkInfo = styled.div`
-    display: flex;
-    justify-content: space-between;
+  display: flex;
+  justify-content: space-between;
 `;
 
 const StoreInfo = styled.div`
-    display: flex;
-    flex-direction: column;
-    gap: 2px;
+  display: flex;
+  flex-direction: column;
+  gap: 2px;
 `;
 
-const StoreName = styled.div`
-
-`;
+const StoreName = styled.div``;
 
 const RateInfo = styled.div`
-    display: flex;
-    gap: 5px;
-    align-items: center;
-    justify-content: center;
+  display: flex;
+  gap: 5px;
+  align-items: center;
+  justify-content: center;
 `;
 
 const Rate = styled.div`
-    padding-top: 4px;
+  padding-top: 4px;
 `;
 
 const StoreCategory = styled.div`
@@ -46,28 +44,30 @@ const StoreCategory = styled.div`
   padding-top: 3px;
 `;
 
-const StoreItem = ({store}) => {
-    const navigate = useNavigate();
-    const handleEnterStore = () => {
-        navigate(`${store.restaurantId}`);
-    };
+const StoreItem = ({ store }) => {
+  const navigate = useNavigate();
+  const handleEnterStore = () => {
+    navigate(`${store.restaurantId}`);
+  };
 
-    return (
-        <div onClick={handleEnterStore} type="button">
-            <StoreImage src={store.imageUrl}/>
-                <BookmarkInfo>
-                    <StoreInfo>
-                        <StoreName>{store.restaurantName}</StoreName>
-                        <RateInfo>
-                            <StarIcon />
-                            <Rate>{store.starCount}</Rate>
-                            <StoreCategory>{store.category}, {store.region}</StoreCategory>
-                        </RateInfo>
-                    </StoreInfo>
-                    <BookmarkIcon2 />
-            </BookmarkInfo>
-        </div>
-    )
-}
+  return (
+    <div onClick={handleEnterStore} type="button">
+      <StoreImage src={store.imageUrl} />
+      <BookmarkInfo>
+        <StoreInfo>
+          <StoreName>{store.restaurantName}</StoreName>
+          <RateInfo>
+            <StarIcon />
+            <Rate>{store.starCount.toFixed(1)}</Rate>
+            <StoreCategory>
+              {store.category}, {store.region}
+            </StoreCategory>
+          </RateInfo>
+        </StoreInfo>
+        <BookmarkIcon2 />
+      </BookmarkInfo>
+    </div>
+  );
+};
 
 export default StoreItem;
