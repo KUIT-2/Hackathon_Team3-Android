4edf1254454c5acf5429a6a5c7d3cfca77cfdea1
nangniya <apinksky00@gmail.com>
2023-12-20 00:12:28 +0900

    feat: API 연동

--- hackathon_team3-web/src/components/BookingCalender/BookingCalender.styles.jsx
+++ hackathon_team3-web/src/components/BookingCalender/BookingCalender.styles.jsx
@@ -1,72 +1,71 @@
 import styled from "styled-components";
 
 export const BookingCalenderHeader = styled.div`
-    display: flex;
-`
+  display: flex;
+`;
 
 export const BookingCalenderToday = styled.div`
-    position: absolute;
-    margin-top: 32px;
-    margin-left: 32px;
-    color: #ff3d00;
-    font-size: 14px;
-    text-decoration: underline;
-`
+  position: absolute;
+  margin-top: 32px;
+  margin-left: 32px;
+  color: #ff3d00;
+  font-size: 14px;
+  text-decoration: underline;
+`;
 
 export const CalendarContainer = styled.div`
-    display: flex;
-    justify-content: center;
+  display: flex;
+  justify-content: center;
 
-    .calendar {
-        margin-top: 16px;
-        line-height: 2rem;
-        font-size: 16px;
+  .calendar {
+    margin-top: 16px;
+    line-height: 2rem;
+    font-size: 16px;
 
-        .react-calendar__navigation {
-            display: flex;
-            margin-left: 65px;
-            height: 44px;
-            margin-bottom: 1em;
-            width: 60%;
-            justify-content: center;
-          }
-        .react-calendar__navigation__label > span {
-            font-size: 14px;
-            font-weight: 500;
-            color: #000;
-        }
-        .react-calendar__month-view__weekdays__weekday abbr {
-            text-decoration: none;
-        }
-        .react-calendar__tile--active {
-            width: 15px;
-            height: 50px;
-            color: #fff;
-            background: #ff3d00;
-            border-radius: 100%;
-        }
-`
+    .react-calendar__navigation {
+      display: flex;
+      margin-left: 65px;
+      height: 44px;
+      margin-bottom: 1em;
+      width: 60%;
+      justify-content: center;
+    }
+    .react-calendar__navigation__label > span {
+      font-size: 14px;
+      font-weight: 500;
+      color: #000;
+    }
+    .react-calendar__month-view__weekdays__weekday abbr {
+      text-decoration: none;
+    }
+    .react-calendar__tile--active {
+      width: 15px;
+      height: 50px;
+      color: #fff;
+      background: #ff3d00;
+      border-radius: 100%;
+    }
+  }
+`;
 export const BookingMemberContainer = styled.div`
-    display: flex;
-    width: 100%;
-    overflow-x: auto;
-    white-space: nowrap;
-    margin-top: 20px;
-    margin-left: 24px;
-    gap: 12px;
-`
+  display: flex;
+  width: 100%;
+  overflow-x: auto;
+  white-space: nowrap;
+  margin-top: 20px;
+  margin-left: 24px;
+  gap: 12px;
+`;
 
 export const BookingMember = styled.div`
-    display: flex;
-    padding: 10px;
-    width: 50px;
-    height: 44px;
-    border-radius: 100%;
-    border: 1px solid #D9D9D9;
-    justify-content: center;
-    align-items: center;
-
-    text-size: 14px;
-    color: #222;
-
-`
+  display: flex;
+  padding: 10px;
+  width: 50px;
+  height: 44px;
+  border-radius: 100%;
+  border: 1px solid #d9d9d9;
+  justify-content: center;
+  align-items: center;
+  font-size: 14px;
+  color: #222;
+`;
